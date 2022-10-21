3e21c027f9a5efabf56b21c736bf2efd54a31e5a
João Pedro <joaopedrorochajdias@gmail.com>
2022-10-21 20:43:17 -0300

    .

---
+++ DDD/interface_user.py
@@ -0,0 +1,15 @@
+from DDD_repository import ddd_repository
+from DDD import Destino
+
+
+class interface_user:
+    def __init__(self, ddd_repository: ddd_repository) -> None:
+        self.ddd_repository = ddd_repository
+
+    def exibir_destinos(self) -> list:
+        return self.ddd_repository
+
+    def saida_usuario(self) -> str:
+        ddd = int(input("Diga o ddd: "))
+        destino = self.ddd_repository.obter_destino_pelo_ddd(ddd)
+        return f"Destino: {destino}"
